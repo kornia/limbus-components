28ce9acea9f457239c8b60165141d78a8ea78573
Luis Ferraz <luisferrazc@gmail.com>
2023-02-28 23:43:47 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='limbus-components',
-      version='0.1.2',
+      version='0.1.3',
       description='Components to be used with limbus.',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
